42cdb21fc607467b6baacf4b693be38eda2b420f
Matheus Felipe <matheusfelipetp@outlook.com>
2023-03-06 15:52:30 -0300

    finished project

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const vm = new Vue({
   data: {
     products: [],
     product: false,
+    cart: [],
+    toastMessage: '',
+    activeToast: false,
+    activeCart: false,
   },
   filters: {
     formatedPrice(value) {
@@ -10,6 +14,17 @@ const vm = new Vue({
         style: 'currency',
         currency: 'BRL',
       });
+    },
+  },
+  computed: {
+    cartTotal() {
+      let total = 0;
+      if (this.cart.length) {
+        this.cart.forEach((item) => {
+          total += item.preco;
+        });
+      }
+      return total;
     },
   },
   methods: {
@@ -23,8 +38,77 @@ const vm = new Vue({
       const json = await response.json();
       this.product = json;
     },
+    openModal(id) {
+      this.fetchProduct(id);
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+    closeModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.product = false;
+      }
+    },
+    closeModalCart({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.activeCart = false;
+      }
+    },
+    addItem() {
+      this.product.estoque--;
+      const { id, nome, preco } = this.product;
+      this.cart.push({
+        id,
+        nome,
+        preco,
+      });
+      this.toast(`${nome} adicionado ao carrinho.`);
+    },
+    removeItem(index) {
+      this.cart.splice(index, 1);
+    },
+    checkLocalStorage() {
+      if (localStorage.cart) {
+        this.cart = JSON.parse(localStorage.cart);
+      }
+    },
+    toast(message) {
+      this.toastMessage = message;
+      this.activeToast = true;
+
+      setTimeout(() => {
+        this.activeToast = false;
+      }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if (hash) {
+        this.fetchProduct(hash.replace('#', ''));
+      }
+    },
+    checkStock() {
+      const items = this.cart.filter(({ id }) => id === this.product.id);
+      this.product.estoque -= items.length;
+    },
+  },
+  watch: {
+    product() {
+      document.title = this.product.nome || 'Techno';
+      const hash = this.product.id || '';
+      history.pushState(null, null, `#${hash}`);
+
+      if (this.product) {
+        this.checkStock();
+      }
+    },
+    cart() {
+      localStorage.cart = JSON.stringify(this.cart);
+    },
   },
   created() {
     this.fetchProducts();
+    this.checkLocalStorage();
+    this.router();
   },
 });
